3b7a03023f4b9d872b85a9736b38bf77b472ae36
soon siong lim <l2smasahiro@gmail.com>
2020-02-29 23:27:39 +0800

    change websdk directory and import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './modules/WebSdk/index';
+import './modules/WebSdk';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
